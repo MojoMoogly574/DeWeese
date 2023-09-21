f914030fcd328fd92aa57194893557ad23b0c2e2
Joseph DeWeese <jdeweese@weldtascics.systems>
2023-09-21 14:05:49 -0500

    Initial Commit

---
+++ DeWeese/DeWeeseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeWeeseApp.swift
+//  DeWeese
+//
+//  Created by J. DeWeese on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct DeWeeseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
